14af0c4a344eb796fdba822a5855645d96199081
Ankit Gwari <ankit.gwari@optimusinfo.com>
2024-10-08 10:50:33 +0530

    more queries covered

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -13,8 +13,14 @@ Insert into student values(30, 'Julie', 'Dehradun', '9999123544', 25 );
 Insert into student values(12, 'Becky', 'Pune', '1111123544', 26 );
 Insert into student values(9, 'Alex', 'Kolkata', '9922223334', 22 );
 Insert into student values(10, 'Alex', 'Kerala', '9922223241', 23 );
+--entry without address
+Insert into student( ROLL_NO ,NAME, PHONE, AGE) values(81, 'Tarun', '9944223241', 21 ); 
 Select * from Student where name LIKE 'j%';
 Select * from Student Order by age ;
 SELECT DISTINCT(AGE),name  FROM Student  ;
 Select * from Student where name LIKE 'j%' AND ADDRESS like 'd%';
 Select * from Student where name LIKE 'j%' OR ADDRESS like 'd%';
+Select * from Student where not name like 'a%';
+Select * from Student where address not in ('Delhi','pune');
+select * from Student;
+Select* from Student where ADDRESS is not NULL;
---
+++ SQLQuery2.sql
@@ -0,0 +1,48 @@
+use StudentDB;
+--see table
+Select * from Student;
+--update query
+UPDATE Student set ADDRESS = 'Gurgaon'
+where ADDRESS = 'Haryana';
+Update student set roll_no = 76 where roll_no = 87; 
+--delete query
+DELETE Student where ROLL_NO = 9;
+--select top few records(number or percent) only
+SELECT TOP 3 * FROM Student;
+SELECT TOP 60 percent * FROM Student;
+--adding marks coulmn
+ALTER table student add marks int;
+--adding marks 
+update student set marks = 90
+where roll_no = 10;
+update student set marks = 30
+where roll_no = 12;
+update student set marks = 85
+where roll_no = 25;
+update student set marks = 52
+where roll_no = 30;
+update student set marks = 53
+where roll_no = 76;
+update student set marks = 35
+where roll_no = 81;
+--aggregate functions
+select count(*) as [Number of Student], ADDRESS 
+from Student group by ADDRESS; 
+select min(marks) from Student;
+select max(marks) from Student;
+SELECT SUM(marks)/count(*) as [Avg marks] 
+FROM Student;
+SELECT AVG(marks) as [Avg marks] 
+FROM Student;
+Select Count(*) as [Passed student] from student where marks>33;
+--like
+SELECT * FROM Student
+WHERE NAME LIKE '[a-f]%';
+SELECT * FROM Student
+WHERE NAME LIKE '[bjt]%';
+--in
+SELECT * FROM Student
+WHERE ADDRESS IN ('Pune', 'dehradun');
+--in
+SELECT * From student where marks between 50 and 80;
+SELECT * From student where marks not between 50 and 80;
